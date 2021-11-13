acae7b5aa0f15ea9629422b8ea7ca540c792959c
FuzzyMistborn <fuzzymistborn@protonmail.com>
2021-11-13 01:05:31 -0500

    test skopeo

--- update.sh
+++ update.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-sudo apt install skopeo
-
 docker_version=$(skopeo inspect docker://docker.io/nextcloud:latest | grep -o 'NEXTCLOUD_VERSION=[0-9]*.[0-9]*.[0-9]*' | cut -c 19-30)
 
 sed -i "/nextcloud/s/.*/FROM nextcloud:$docker_version/" Dockerfile
